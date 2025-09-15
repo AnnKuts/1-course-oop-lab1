cc4fa5c32dfe46b9d602d85f3aa30f89cdaaf703
Ann Kuts <kuutsann@gmail.com>
2025-09-15 17:34:22 +0300

    feat: add Input component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Button = ({ children, onClick, type = 'button', className = '' }) => {
     return (
-        <button type={type} onClick={onClick} className={`button ${className}`}>
+        <button type={type}
+                onClick={onClick}
+                className={`button ${className}`}>
             {children}
         </button>
     );
